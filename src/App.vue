73f49f66b95af8be97376d49619da1f7c9273060
승섭kim <mcar100@naver.com>
2023-11-01 18:58:21 +0900

    과제1: 배열을 이용해 데이터바인딩 해보기

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>원룸샵</div>
   <div>
-    원룸샵
-    <h4 :style="스타일">xx 원룸</h4>
-    <p>{{ price1 }} 만원</p>
+    <h4>{{ products[0] }}</h4>
+    <p>{{ prices[0] }}만원</p>
   </div>
   <div>
-    <h4>xx 원룸</h4>
-    <p>{{ price2 }} 만원</p>
+    <h4>{{ products[1] }}</h4>
+    <p>{{ prices[1] }}만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>{{ prices[2] }}만원</p>
   </div>
 </template>
 
@@ -16,9 +19,8 @@ export default {
   name: "App",
   data() {
     return {
-      price1: 60,
-      price2: 70,
-      스타일: "color: blue",
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+      prices: [60, 70, 50],
     };
   },
   components: {},
@@ -33,5 +35,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
