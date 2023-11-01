5202bede8a542f478d714ce5d4401c2bad589842
승섭kim <mcar100@naver.com>
2023-11-01 21:06:52 +0900

    이미지 추가

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,33 @@
     <a v-for="n in navigator" :key="n">{{ n }}</a>
   </div>
 
-  <div v-for="(product, idx) in products" :key="idx">
+  <!-- <div v-for="(product, idx) in products" :key="idx">
     <h4>{{ product.name }}</h4>
     <p>{{ product.price }} 만원</p>
     <button v-on:click="increase(idx)">허위매물신고</button>
-    <span>신고수: {{ product.count }}</span>
-  </div>
-  <!-- <div>
+    <span> 신고수: {{ product.count }}</span>
+  </div> -->
+  <div>
+    <img src="./assets/room0.jpg" class="room-img" />
     <h4>{{ products[0].name }}</h4>
     <h4>{{ products[0].price }}</h4>
-    <button v-on:click="increase">허위매물신고</button>
-    <span>신고수: {{ count }}</span>
+    <button v-on:click="products[0].count++">허위매물신고</button>
+    <span>신고수: {{ products[0].count }}</span>
   </div>
   <div>
+    <img src="./assets/room1.jpg" class="room-img" />
     <h4>{{ products[1].name }}</h4>
     <h4>{{ products[1].price }}</h4>
+    <button v-on:click="products[1].count++">허위매물신고</button>
+    <span> 신고수: {{ products[1].count }}</span>
   </div>
   <div>
+    <img src="./assets/room2.jpg" class="room-img" />
     <h4>{{ products[2].name }}</h4>
     <h4>{{ products[2].price }}</h4>
-  </div> -->
+    <button v-on:click="products[2].count++">허위매물신고</button>
+    <span> 신고수: {{ products[2].count }}</span>
+  </div>
 </template>
 
 <script>
@@ -33,9 +40,9 @@ export default {
       // count: 0,
       navigator: ["Home", "Shop", "About"],
       products: [
-        { name: "역삼동원룸", price: 60, count: 0 },
-        { name: "천호동원룸", price: 70, count: 0 },
-        { name: "마포구원룸", price: 50, count: 0 },
+        { name: "역삼동원룸", price: 60, count: 0, url: "./assets/room0.jpg" },
+        { name: "천호동원룸", price: 70, count: 0, url: "./assets/room1.jpg" },
+        { name: "마포구원룸", price: 50, count: 0, url: "./assets/room2.jpg" },
       ],
     };
   },
@@ -66,5 +73,10 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 20px;
 }
 </style>
