7525afafe8bcae17fa95ee287b7f4e990dc9ecc5
승섭kim <mcar100@naver.com>
2023-11-01 18:32:19 +0900

    데이터바인딩1: data 객체 안에 데이터를 넣고, {{ }}로 데이터를 가져올 수 있다

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <div>
+    원룸샵
+    <h4>xx 원룸</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>xx 원룸</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
 </template>
 
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      price1: 60,
+      price2: 70,
+    };
+  },
   components: {},
 };
 </script>
