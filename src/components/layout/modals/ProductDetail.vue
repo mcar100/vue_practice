eefc418803f402987056c38f7186e90b24993587
승섭kim <mcar100@naver.com>
2023-11-09 17:28:29 +0900

    Style: 상품 모달창 스타일 조정

--- src/components/layout/modals/ProductDetail.vue
+++ src/components/layout/modals/ProductDetail.vue
@@ -2,10 +2,12 @@
   <div class="black-bg">
     <div class="white-bg">
       <button @click="callCloseModal" class="close-btn">x</button>
-      <h4>{{ modal.title }}</h4>
-      <img :src="modal.image" class="modal-img" />
-      <p>{{ modal.price }}</p>
-      <p>{{ modal.content }}</p>
+      <div class="modal-container">
+        <h4>{{ modal.title }}</h4>
+        <img :src="modal.image" class="modal-img" />
+        <p>{{ modal.price }}</p>
+        <p>{{ modal.content }}</p>
+      </div>
     </div>
   </div>
 </template>
