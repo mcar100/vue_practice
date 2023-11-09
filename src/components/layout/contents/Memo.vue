a7a3420b6c19fd7149d639a20a9287071aa1a2be
승섭kim <mcar100@naver.com>
2023-11-09 17:22:47 +0900

    Style: Memo 게시판, Memo 컴포넌트 레이아웃 구성

---
+++ src/components/layout/contents/Memo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="memo memo-type4">
+    <div class="memo-head">
+      <div class="memo-pin"></div>
+    </div>
+    <div class="memo-body">
+      <textarea readonly></textarea>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "MemoItem",
+  method: {},
+  components: {},
+};
+</script>
+<style lang="scss"></style>
--- src/views/ToDoList.vue
+++ src/views/ToDoList.vue
@@ -1,11 +1,30 @@
 <template>
-  <div>
-    <h1>To Do List Page</h1>
+  <div class="line-container">
+    <div class="nail"></div>
+    <div class="line"></div>
+    <div class="line"></div>
+  </div>
+  <div class="board-container">
+    <div class="memo-container">
+      <MemoItem />
+    </div>
+  </div>
+  <div class="input-container">
+    <input
+      type="text"
+      maxlength="30"
+      placeholder="해야 할 일을 입력해주세요 (30자 이내)"
+    />
+    <button type="submit">등록</button>
   </div>
 </template>
 <script>
+import MemoItem from "@/components/layout/contents/Memo.vue";
 export default {
   name: "ToDoListPage",
+  components: {
+    MemoItem,
+  },
 };
 </script>
 <style lang="scss"></style>
