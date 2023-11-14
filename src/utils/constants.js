55c14433897fbb9e0a14453bc20c42db160f0c50
승섭kim <mcar100@naver.com>
2023-11-14 18:49:30 +0900

    Feat: 상수 추가

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+// memo mode: normal, creating, moving
+const MODE_NORMAL = 0;
+const MODE_CREATING = 1;
+const MODE_MOVING = 2;
+
+// memo max length;
+const MEMO_MAXLENGTH = 8;
+
+export { MODE_NORMAL, MODE_CREATING, MODE_MOVING, MEMO_MAXLENGTH };
